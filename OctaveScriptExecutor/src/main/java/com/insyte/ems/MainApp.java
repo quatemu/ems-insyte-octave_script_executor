eff01421913a7fd4d61bbcc4019abdbf02627cac
Artem <kuchumov7@gmail.com>
2017-11-06 22:53:16 +0500

    First commit

---
+++ OctaveScriptExecutor/src/main/java/com/insyte/ems/MainApp.java
@@ -0,0 +1,8 @@
+package com.insyte.ems;
+
+public class MainApp {
+    public static void main(String[] args){
+        System.out.println("Hello, world");
+    }
+}
+
---
+++ OctaveScriptExecutor/src/main/java/com/insyte/ems/utils/executors/OctaveExecutor.java
@@ -0,0 +1,8 @@
+package com.insyte.ems.utils.executors;
+
+public class OctaveExecutor {
+
+    public void f(){
+
+    }
+}
